76fe2049ecf783819b97195bebb7928886e89c0f
andygarcia99sgt <andygabrielxicongarcia@gmail.com>
2019-06-04 17:50:56 -0600

    version3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-var pokemon = angular.module("pokemon",[])
+var Pokemon = angular.module("Pokemon",[])
 
-pokemon.controller("ListadoPokemon",function($scope,$rooScope,$http){
+Pokemon.controller("ListadoPokemon",function($scope,$rootScope,$http){
 	$scope.Pokebola = [];
+	for(var M = 1; M <=500;M++){
+
+	
 	$http({
 		method: "GET",
-		url: "https://pokeapi.co/api/v2/pokemon/55"
+		url: "https://pokeapi.co/api/v2/pokemon/" + M
 	}).then(function success(x){
-		$scoope.pokebola.push(x);
+		$scope.Pokebola.push(x);
 	})
+}	
 })
+
+ /*01000001 01101110 01100100 01111001 00100000 01111000 01101001 01100011 01101111 01101110 */
